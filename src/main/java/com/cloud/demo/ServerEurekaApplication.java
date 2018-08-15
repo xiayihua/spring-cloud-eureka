28e7256202937d5b45fa2c9b494f35c206fa2f39
xiayihua <xiayihua8392@126.com>
2018-08-15 22:29:37 +0800

    提交测试

--- src/main/java/com/cloud/demo/ServerEurekaApplication.java
+++ src/main/java/com/cloud/demo/ServerEurekaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ServerEurekaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServerEurekaApplication.class, args);
 	}
